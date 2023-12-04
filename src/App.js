feda18fa882b7b4bdd7f125ede242589cda1026d
FAISAL <125458443+faisalqashmrfs@users.noreply.github.com>
2023-12-04 09:54:48 -0500

    up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import MyNavbar from  './assets/component/MyNavbar'
 import './style.css';
-import {Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Explore from "./pages/Explore/Explore";
 import Blog from "./pages/Blog/Blog";
@@ -9,6 +8,7 @@ import Contact from "./pages/Contact/Contact";
 import Gallery from "./pages/Gallery/Gallery";
 import BookingSection from "./pages/Booking/BookingSection";
 import TheFooter from "./assets/component/TheFooter";
+import { Route, Routes } from "react-router-dom";
 
 function App()
 {
@@ -16,12 +16,12 @@ function App()
       <>
       <MyNavbar />
       <Routes>
-      <Route path="" element={<Home />} />
-      <Route path="/blog"  element={<Blog />} />
-      <Route path="/contact"  element={<Contact />} />
-      <Route path="/gallery"  element={<Gallery />} />
-      <Route path="/explore"  element={<Explore />} />
-      <Route path="/booking"  element={<BookingSection />} />
+        <Route path="/" element={<Home/>}/>
+        <Route path="/blog"  element={<Blog/>}/>
+        <Route path="/contact"  element={<Contact/>}/>
+        <Route path="/gallery"  element={<Gallery/>}/>
+        <Route path="/explore"  element={<Explore/>}/>
+        <Route path="/booking"  element={<BookingSection/>}/>
       </Routes>
       <TheFooter/>
       </>
