c1f7376cac9ab5fd6431833c47e9eff7ec38a281
FAISAL <125458443+faisalqashmrfs@users.noreply.github.com>
2024-01-18 05:59:53 -0800

    up

--- src/pages/components/Hero/Hero.jsx
+++ src/pages/components/Hero/Hero.jsx
@@ -1,19 +1,19 @@
 import './Hero.css';
 
 
-function Hero(props){
+function Hero(props) {
   console.log(props.togle)
   return (
-    <div className={ props.togle ? "zh-hero bluuuuer" : "zh-hero"}>
-        <div className='FQ-100height'>
-          <img className='zh-img' src={props.imghero} alt='#'/>
-          { props.shadowHero && <img className='zh-img2'src={props.shadowHero} alt='#'/>}
-          zh-hero    </div>
-        <div className="zh-info">
-            <h1>{props.titlehero}</h1>
-            <p>{props.deschero}</p>
-            {props.scrollimg && <img className='scroling-hero' src={props.scrollimg}/>}
-        </div>
+    <div className={props.togle ? "zh-hero bluuuuer" : "zh-hero"}>
+      <div className='FQ-100height'>
+        <img className='zh-img' src={props.imghero} alt='#' />
+        {props.shadowHero && <img className='zh-img2' src={props.shadowHero} alt='#' />}
+      </div>
+      <div className="zh-info">
+        <h1>{props.titlehero}</h1>
+        <p>{props.deschero}</p>
+        {props.scrollimg && <img className='scroling-hero' src={props.scrollimg} />}
+      </div>
     </div>
   )
 }
