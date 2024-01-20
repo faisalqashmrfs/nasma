d5c7c6e0e668c12606e55f63fb55b72618fa6f5d
FAISAL <125458443+faisalqashmrfs@users.noreply.github.com>
2024-01-20 00:39:43 -0800

    up

--- src/pages/components/imagegray/ImageGray.jsx
+++ src/pages/components/imagegray/ImageGray.jsx
@@ -1,56 +1,62 @@
+import { Link } from 'react-router-dom'
 import './ImageGray.css'
 
 function ImageGray() {
   return (
     <div className='imagesg'>
 
-      <div className='fi'>
-        <div className='firstimg'>
-          <h1>GYM</h1>
+      <Link to="./gallery">
+        <div className='fi'>
+          <div className='firstimg'>
+            <h1>GYM</h1>
+          </div>
+          <div className='mytext'>
+            <h1>GYM</h1>
+            <p>Never stop your daily activity</p>
+          </div>
         </div>
-        <div className='mytext'>
+        <div className='fq-my1'>
+          <div className='shadowwww'>
+          </div>
           <h1>GYM</h1>
           <p>Never stop your daily activity</p>
         </div>
-      </div>
-      <div className='fq-my1'>
-        <div className='shadowwww'>
+      </Link>
+      <Link to="./gallery">
+        <div className='se'>
+          <div className='firstimg'>
+            <h1>POOL</h1>
+          </div>
+          <div className='mytext'>
+
+            <h1>POOL</h1>
+            <p>Outdoor pool & indoor </p>
+          </div>
         </div>
-          <h1>GYM</h1>
-          <p>Never stop your daily activity</p>
-        </div>
-      <div className='se'>
-        <div className='firstimg'>
-          <h1>POOL</h1>
-        </div>
-        <div className='mytext'>
-        
-          <h1>POOL</h1>
-          <p>Outdoor pool & indoor </p>
-        </div>
-      </div>
-      <div className='fq-my2'>
-      <div className='shadowwww'>
-        </div>
+        <div className='fq-my2'>
+          <div className='shadowwww'>
+          </div>
           <h1>POOL</h1>
           <p>Outdoor pool & indoor </p>
         </div>
-      <div className='th'>
-        <div className='firstimg'>
-          <h1>SPA</h1>
+      </Link>
+      <Link to="./gallery">
+        <div className='th'>
+          <div className='firstimg'>
+            <h1>SPA</h1>
+          </div>
+          <div className='mytext'>
+            <h1>SPA</h1>
+            <p>Relax in our massage room</p>
+          </div>
         </div>
-        <div className='mytext'>
-          <h1>SPA</h1>
-          <p>Relax in our massage room</p>
-        </div>
-      </div>
-      <div className='fq-my3'>
-      <div className='shadowwww'>
-        </div>
+        <div className='fq-my3'>
+          <div className='shadowwww'>
+          </div>
           <h1>SPA</h1>
           <p>Relax in our massage room </p>
         </div>
-
+      </Link>
     </div>
   )
 }
